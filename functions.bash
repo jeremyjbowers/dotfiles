75249a3bce6a359fae71d7450e83eb0991049f3b
Danny DeBelius <danny.debelius@gmail.com>
2013-05-02 15:35:26 -0400

    Add Base64 encode function

--- functions.bash
+++ functions.bash
@@ -124,3 +124,13 @@ function extract() {
 		echo "'$1' is not a valid file"
 	fi
 }
+
+# Create a data URI from a file and copy it to the pasteboard
+# Credit to https://github.com/necolas/dotfiles/blob/master/bash/functions/datauri
+datauri() {
+    local mimeType=$(file -b --mime-type "$1")
+    if [[ $mimeType == text/* ]]; then
+        mimeType="${mimeType};charset=utf-8"
+    fi
+    printf "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')" | pbcopy | printf "=> data URI copied to pasteboard.\n"
+}
